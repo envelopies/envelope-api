3ce5b38f7b962a9eb178256f5b202be618e9f610
dimon4ezzz <37621957+dimon4ezzz@users.noreply.github.com>
2024-10-21 18:27:45 +0500

    feat: date time formatter improvement

--- src/main/kotlin/ru/envelope/api/mappers/ItemMapper.kt
+++ src/main/kotlin/ru/envelope/api/mappers/ItemMapper.kt
@@ -6,11 +6,13 @@ import java.time.format.DateTimeFormatter
 import java.util.function.Function
 
 object ItemMapper: Function<Item, ItemDto> {
+    private val localDateTimeFormatter: DateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss")
+
     override fun apply(item: Item): ItemDto = ItemDto(
         id = item.id?.toString() ?: "",
         title = item.title,
         description = item.description,
         price = item.price,
-        createdAt = item.createdAt.format(DateTimeFormatter.ISO_LOCAL_DATE_TIME)
+        createdAt = item.createdAt.format(localDateTimeFormatter)
     )
 }
