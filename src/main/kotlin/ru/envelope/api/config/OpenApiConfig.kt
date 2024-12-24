d0870146293cef57d8285849bda3eeea07fb3fe4
dimon4ezzz <37621957+dimon4ezzz@users.noreply.github.com>
2024-12-24 23:14:20 +0500

    feat: указание конкретного адреса сервера OpenApi

--- src/main/kotlin/ru/envelope/api/config/OpenApiConfig.kt
+++ src/main/kotlin/ru/envelope/api/config/OpenApiConfig.kt
@@ -3,6 +3,7 @@ package ru.envelope.api.config
 import io.swagger.v3.oas.models.Components
 import io.swagger.v3.oas.models.OpenAPI
 import io.swagger.v3.oas.models.security.SecurityScheme
+import io.swagger.v3.oas.models.servers.Server
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
@@ -10,6 +11,7 @@ import org.springframework.context.annotation.Configuration
 class OpenApiConfig {
     @Bean
     fun createOpenAPI(): OpenAPI = OpenAPI()
+        .servers(listOf(Server().url("https://nvk1.store/api")))
         .components(Components()
             .addSecuritySchemes("default", SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")))
 }
