4eb9b414f9f7e764170dcb121d20c4063416a773
dimon4ezzz <37621957+dimon4ezzz@users.noreply.github.com>
2025-05-04 01:38:10 +0500

    tests: добавление тестов

---
+++ src/test/kotlin/ru/envelope/api/controllers/ItemControllerTests.kt
@@ -0,0 +1,30 @@
+package ru.envelope.api.controllers
+
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.context.annotation.Import
+import org.springframework.http.MediaType
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.get
+import ru.envelope.api.PostgresTestcontainersConfiguration
+
+@AutoConfigureMockMvc
+@Import(PostgresTestcontainersConfiguration::class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class ItemControllerTests {
+    @Autowired
+    private lateinit var mockMvc: MockMvc
+
+    @Test
+    fun `test items list`() {
+        mockMvc.get("/v1/items") {
+
+        }.andExpect {
+            status { isOk() }
+            content { contentType(MediaType.APPLICATION_JSON) }
+            content { json("""[]""") }
+        }
+    }
+}
