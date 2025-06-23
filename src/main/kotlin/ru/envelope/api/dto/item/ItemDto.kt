6f0cdb5a8b5dc248d04bccacc8435d1c4ca11ddf
dimon4ezzz <37621957+dimon4ezzz@users.noreply.github.com>
2025-06-23 22:54:46 +0500

    docs: улучшение OpenAPI docs

--- src/main/kotlin/ru/envelope/api/dto/item/ItemDto.kt
+++ src/main/kotlin/ru/envelope/api/dto/item/ItemDto.kt
@@ -6,27 +6,27 @@ data class ItemDto(
     @Schema(description = "ID товара")
     val id: String,
 
-    @Schema(description = "название товара")
+    @Schema(description = "название товара", example = "лит энерджи")
     val title: String,
 
-    @Schema(description = "описание товара")
+    @Schema(description = "описание товара", example = "бодрящий напиток")
     val description: String,
 
-    @Schema(description = "цена товара в наиболее дробных единицах (копейках)")
+    @Schema(description = "цена товара в наиболее дробных единицах (копейках)", example = "12000")
     val price: Int,
 
     @Schema(description = "дата и время публикации", format = "yyyy-MM-dd'T'HH:mm:ss")
     val createdAt: String,
 
-    @Schema(description = "имя пользователя, создавшего товар")
+    @Schema(description = "имя пользователя, создавшего товар", example = "jointplayer")
     val username: String?,
 
-    @Schema(description = "категория, к которой относится товар")
+    @Schema(description = "категория, к которой относится товар", example = "c5c6f33f-cbdd-4737-8bdb-e5c35ea03709")
     val category: String?,
 
-    @Schema(description = "список мест доставки")
+    @Schema(description = "список мест доставки", example = "[\"НВК 4\", \"НВК 5\"]")
     val deliveryAddresses: List<String>,
 
-    @Schema(description = "изображения к товару")
+    @Schema(description = "изображения к товару", example = "[\"28781ae4-2d90-4757-aaa3-61fe2ee9ca0e\", \"dbb8c6b2-fded-4a91-94ba-f181b3455b48\"]")
     val pictures: List<String>
 )
--- src/main/kotlin/ru/envelope/api/dto/item/ItemPostDto.kt
+++ src/main/kotlin/ru/envelope/api/dto/item/ItemPostDto.kt
@@ -4,21 +4,21 @@ import io.swagger.v3.oas.annotations.media.Schema
 import java.util.UUID
 
 data class ItemPostDto(
-    @Schema(description = "название товара")
+    @Schema(description = "название товара", example = "лит энерджи")
     val title: String,
 
-    @Schema(description = "описание товара")
+    @Schema(description = "описание товара", example = "бодрящий напиток")
     val description: String,
 
-    @Schema(description = "цена товара в наиболее дробных единицах (копейках)")
+    @Schema(description = "цена товара в наиболее дробных единицах (копейках)", example = "12000")
     val price: Int,
 
-    @Schema(description = "идентификатор категории, к которой относится товар")
+    @Schema(description = "идентификатор категории, к которой относится товар", example = "c5c6f33f-cbdd-4737-8bdb-e5c35ea03709")
     val categoryId: UUID,
 
-    @Schema(description = "список UUID мест доставки")
+    @Schema(description = "список UUID мест доставки", example = "[\"73425643-23d2-4188-b32c-214f3a688411\", \"8b0a31f1-0158-4397-9dc2-a7a5c1243589\"]")
     val deliveryAddresses: List<UUID>?,
 
-    @Schema(description = "список UUID картинок")
+    @Schema(description = "список UUID картинок", example = "[\"28781ae4-2d90-4757-aaa3-61fe2ee9ca0e\", \"dbb8c6b2-fded-4a91-94ba-f181b3455b48\"]")
     val pictures: List<UUID>?
 )
